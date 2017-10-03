4ca843fd4825815fd4ccc5aa4873c068049868fa
crowcasso <crowcasso@gmail.com>
2017-10-03 12:51:42 -0400

    MWF Geotify

---
+++ Geotify/AddGeotificationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddGeotificationViewController.swift
+//  Geotify
+//
+//  Created by Joel Hollingsworth on 10/2/17.
+//  Copyright © 2017 Joel Hollingsworth. All rights reserved.
+//
+
+import UIKit
+
+class AddGeotificationViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func onCancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
